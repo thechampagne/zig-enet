6dc49ab28d25750249d0669519afd9ed07c46de7
Dagoth Gares <63978287+DagothGares@users.noreply.github.com>
2023-08-22 09:40:53 -0500

    feat: allow choosing to build zig-enet as a static or shared library
    Note that enet itself will still be built directly into zig-enet as a
    static library, regardless.

--- build.zig
+++ build.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
 
+const build_type = enum {
+    static,
+    shared,
+};
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -10,13 +15,27 @@ pub fn build(b: *std.Build) void {
     });
     const enet = enet_dep.artifact("libenet");
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-enet",
-        .root_source_file = .{ .path = "src/enet.zig" },
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
+    const lib_type = b.option(
+        build_type,
+        "build_type",
+        "Whether you wish to build zig-enet as a static or shared library.",
+    ) orelse .static;
+
+    const lib: *std.Build.Step.Compile = switch (lib_type) {
+        .static => b.addStaticLibrary(.{
+            .name = "zig-enet",
+            .root_source_file = .{ .path = "src/enet.zig" },
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+        .shared => b.addSharedLibrary(.{
+            .name = "zig-enet",
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    };
     lib.linkLibrary(enet);
     lib.installLibraryHeaders(enet);
     lib.addIncludePath(.{ .path = "." });
