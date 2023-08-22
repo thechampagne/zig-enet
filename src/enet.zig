3e9f1bce70370f94db61d6808de404aedacf2583
Dagoth Gares <63978287+DagothGares@users.noreply.github.com>
2023-08-22 09:50:25 -0500

    feat: add init_callbacks to ENet struct; add InitError error type
    also adds ENet.MallocFn, ENet.FreeFn, ENet.NoMemFn types to help
    visualize the intended inputs for enet.ENetCallbacks

--- src/enet.zig
+++ src/enet.zig
@@ -10,16 +10,45 @@ pub const ENet = struct {
     ref_count: u16 = 0,
     mutex: std.Thread.Mutex = .{},
 
+    // TODO: put these in a helper struct for enet.ENetCallbacks instead
+    pub const MallocFn = ?*const fn (usize) callconv(.C) ?*anyopaque;
+    pub const FreeFn = ?*const fn (?*anyopaque) callconv(.C) void;
+    pub const MoNemFn = ?*const fn () callconv(.C) void;
+
+    pub const InitError = error{ ENet_Initialization_Failure, ENet_Already_Initialized };
+
     const Self = @This();
 
-    pub fn init() !*Self {
+    pub fn init() InitError!*Self {
         enet_handle.mutex.lock();
         defer enet_handle.mutex.unlock();
 
         if (enet_handle.ref_count == 0) {
             if (enet.enet_initialize() != 0) {
-                return error.ENet_Initialization_Failure;
+                return InitError.ENet_Initialization_Failure;
             }
+        }
+
+        enet_handle.ref_count += 1;
+
+        return &enet_handle;
+    }
+
+    /// This function will error if ENet is already initialized.
+    /// TODO: provide a structure for setting ENetCallbacks from the user's choice of allocator
+    pub fn init_callbacks(
+        version: enet.ENetVersion,
+        inits: *const enet.ENetCallbacks,
+    ) InitError!*Self {
+        enet_handle.mutex.lock();
+        defer enet_handle.mutex.unlock();
+
+        if (enet_handle.ref_count > 0) {
+            return InitError.ENet_Already_Initialized;
+        }
+
+        if (enet.enet_initialize_with_callbacks(version, inits) != 0) {
+            return InitError.ENet_Initialization_Failure;
         }
 
         enet_handle.ref_count += 1;
